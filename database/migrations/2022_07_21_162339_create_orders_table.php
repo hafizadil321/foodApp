ee4c2c641f0d59d204006e435530c1fba3d2ceb4
Usman Adil <usmanadil431@gmail.com>
2022-07-22 18:12:10 +0500

    new table

--- database/migrations/2022_07_21_162339_create_orders_table.php
+++ database/migrations/2022_07_21_162339_create_orders_table.php
@@ -22,13 +22,13 @@ class CreateOrdersTable extends Migration
             $table->string('billing_email');
             $table->string('billing_address');
             $table->string('billing_phone');
-            $table->string('billing_name_on_card');
+            $table->string('billing_name_on_card')->nullable();
             $table->string('billing_discount')->default(0);
             $table->string('billing_discount_code')->nullable();
-            $table->string('billing_subtotal');
-            $table->string('billing_tax');
-            $table->string('billing_total');
-            $table->string('payment_gateway');
+            $table->string('billing_subtotal')->nullable();
+            $table->string('billing_tax')->nullable();
+            $table->string('billing_total')->nullable();
+            $table->string('payment_gateway')->nullable();
             $table->boolean('payment_status')->comment('0=Pending,1=Complete')->nullable();
             $table->timestamps();
         });
